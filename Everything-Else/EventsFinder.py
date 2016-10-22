6d3c608010e338bf2601fdc0b83eb776c1555605
copperstick6 <copperstick6@gmail.com>
2016-10-22 15:59:03 -0500

    small try catch thing

--- Everything-Else/EventsFinder.py
+++ Everything-Else/EventsFinder.py
@@ -1,1 +1,11 @@
 import keys
+import urllib.request
+import json
+import codecs
+
+def getEvents():
+    s = "https://api.sigimera.org/v1/crises?auth_token="
+    s+= keys.sigimeraAuth()
+    webUrl  = urllib.request.urlopen(s)
+    if(webUrl.getcode() == 200)
+        response = webUrl.read().decode('utf-8')
