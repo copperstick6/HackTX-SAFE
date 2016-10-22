26cb27b35720422ab8395b95a7f9aeeb442186fd
copperstick6 <copperstick6@gmail.com>
2016-10-22 14:12:23 -0500

    finished up google maps distance

--- Everything-Else/googleMapsDistance.py
+++ Everything-Else/googleMapsDistance.py
@@ -1,11 +1,25 @@
 import keys
 import urllib.request
 import json
+import pprint
+import sys
 
-def getWebUrl(address 1, address 2):
 
-if (webUrl.getcode() == 200):
-
+def getWebUrl(address1, address2):
+    s = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+    for i in str(address1):
+        if(i = " "):
+            tempS+="+"
+        else:
+            tempS+=
+    s+= tempS + "&destinations="
+    for i in str(address2):
+        if(i = " "):
+            tempS2+="+"
+        else:
+            tempS2+=
+    s+=tempS + "&key=" + key.GoogleMapsKey
+    return s
 
 def getDistance(address1, address2):
     webUrl = urllib2.urlrequest.urlopen(getWebUrl(address1, address2))
