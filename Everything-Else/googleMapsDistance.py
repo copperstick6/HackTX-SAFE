b2ab17458b170fce355cd47f8b77f88a79c29df0
copperstick6 <copperstick6@gmail.com>
2016-10-22 14:52:23 -0500

    finished distances.

--- Everything-Else/googleMapsDistance.py
+++ Everything-Else/googleMapsDistance.py
@@ -3,29 +3,35 @@ import urllib.request
 import json
 import pprint
 import sys
-
+import codecs
 
 def getWebUrl(address1, address2):
-    s = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+    s = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
+    tempS = ""
+    tempS2 = ""
     for i in str(address1):
-        if(i = " "):
+        if(i == " "):
             tempS+="+"
         else:
-            tempS+=
+            tempS+= i
     s+= tempS + "&destinations="
     for i in str(address2):
-        if(i = " "):
+        if(i == " "):
             tempS2+="+"
         else:
-            tempS2+=
-    s+=tempS + "&key=" + key.GoogleMapsKey
+            tempS2+= i
+    tempKey = keys.GoogleMapsKey()
+    s+=tempS2 + "&key=" + tempKey
     return s
 
 def getDistance(address1, address2):
-    webUrl = urllib2.urlrequest.urlopen(getWebUrl(address1, address2))
+    webUrl1 = urllib.request.urlopen(getWebUrl(address1, address2)).read().decode('utf8')
+    webUrl = urllib.request.urlopen(getWebUrl(address1, address2))
     if (webUrl.getcode() == 200):
-        data = webUrl.read()
-        theJSON = json.loads(data)
-        s =theJSON["rows"]["elements"]["distance"]
-        s = s[:-3]
+        str_response = webUrl.read().decode('utf-8')
+        obj = json.loads(str_response)
+        s =obj["rows"][0]["elements"][0]["distance"]["value"]
         return s
+
+#distance in meters
+print(getDistance("6813 Beverly Glen Drive", "201 East 21st Street"))
