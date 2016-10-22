f4ca6f4d1e314ee951984ea17492fd99a933255a
copperstick6 <copperstick6@gmail.com>
2016-10-22 16:02:31 -0500

    added some stuff with eventsFinder

--- Everything-Else/EventsFinder.py
+++ Everything-Else/EventsFinder.py
@@ -3,9 +3,10 @@ import urllib.request
 import json
 import codecs
 
-def getEvents():
+def getEvents(list1):
     s = "https://api.sigimera.org/v1/crises?auth_token="
     s+= keys.sigimeraAuth()
     webUrl  = urllib.request.urlopen(s)
     if(webUrl.getcode() == 200)
         response = webUrl.read().decode('utf-8')
+        theJSON = json.loads(response)
