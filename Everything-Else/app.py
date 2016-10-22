6012c1857ef30be77bfbad67701d9c3a6581e219
sjf25 <sohaibjf@gmail.com>
2016-10-22 18:13:42 -0500

    updated app.py can signup, login, and logout by email

--- Everything-Else/app.py
+++ Everything-Else/app.py
@@ -6,6 +6,7 @@ from wtforms import StringField, PasswordField, SubmitField, SelectField, valida
 from werkzeug.security import generate_password_hash, check_password_hash
 from twilio.rest import TwilioRestClient
 from flask_mail import Mail, Message
+from flask_security import login_required
 import random
 import keys
 import sys
@@ -39,7 +40,16 @@ class RegisterForm(FlaskForm):
 	phone_number = StringField('Phone Number', [required_if_using_text])
 	submit = SubmitField('Sign Up')
 
-required_if_using_text_caller = required_if_using_text
+# The signup form
+# Again, feel free to change the label values
+class LoginForm(FlaskForm):
+	email = StringField('Email', [validators.DataRequired('Email is required')])
+	password = PasswordField('Password', [validators.DataRequired('Password is required')])
+	submit = SubmitField('Login')
+
+# User class (used for logging in)
+class User(flask_login.UserMixin):
+	pass
 
 # set up the app
 app = Flask(__name__)
@@ -54,6 +64,16 @@ app.config['MONGO_DBNAME'] = 'safe_db'
 mongo = PyMongo(app)
 
 # email stuff
+gmail_username = 'safe2faconfirm@gmail.com'
+# TODO turn off DEBUG when in production
+app.config.update(
+	DEBUG = True,
+	MAIL_SERVER = 'smtp.gmail.com',
+	MAIL_PORT = 465,
+	MAIL_USE_SSL = True,
+	MAIL_USERNAME = gmail_username,
+	MAIL_PASSWORD = keys.gmail_password()
+	)
 mail = Mail(app)
 
 # flashes form errors
@@ -104,13 +124,61 @@ def send_verification_text(unique_code, address):
 		from_ = keys.phoneNumber())
 
 # send verification email
-# TODO fix this
 def send_verification_email(unique_code, address):
 	print("EMAILING", file=sys.stderr)
+	print("ADDRESS:", address)
 	msg_body = "Your verification code is: " + str(unique_code)
-	msg = Message('Verification Code', sender = 'blah@blah.blahblah', recipients = [address])
+	msg = Message('Verification Code', sender = gmail_username, recipients = [address])
 	msg.body = msg_body
 	mail.send(msg)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm(request.form)
+	if request.method == 'POST' and form.validate():
+		email = request.form['email']
+		userRecord = mongo.db.users.find_one({'email': email})
+		if userRecord is not None:
+			user = User()
+			user.id = email
+			flask_login.login_user(user)
+			return redirect(url_for('index'))
+	elif request.method == 'POST':
+		flash_errors(form)
+	return render_template('login.html', form = form)
+
+@login_manager.user_loader
+def load_user(email):
+	userRecord = mongo.db.users.find_one({'email': email})
+	# if no user w/ this email exists, return None
+	if userRecord is None:
+		return
+	user = User()
+	user.id = email
+	return user
+
+@login_manager.request_loader
+def request_loader(request):
+	email = request.form.get('email')
+	userRecord = mongo.db.users.find_one({'email': email})
+	# if no user w/ this email exists, return None
+	if userRecord is None:
+		return
+	user = User()
+	user.id = email
+	passwordHash = request.form['password']
+	user.is_authenticated = check_password_hash(passwordHash, password)
+	return user
+
+@app.route('/logout')
+def logout():
+	flask_login.logout_user()
+	return redirect(url_for('index'))
+
+@app.route('/settings')
+@login_required
+def settings():
+	return flask_login.current_user.id
 
 if __name__ == '__main__':
 	app.run()
