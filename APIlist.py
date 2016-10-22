bcbab4cbe44afc35a9576559dee0fd2d0eff4058
copperstick6 <copperstick6@gmail.com>
2016-10-22 10:53:11 -0500

    required APIs

---
+++ APIlist.py
@@ -0,0 +1,30 @@
+#Below is the list of all CONFIRMED APIs we are using.
+#Twilio API: https://www.twilio.com/
+#Google Maps API: https://developers.google.com/maps/documentation/ Python.
+#Crises API: http://api.crisis.net/
+#ReliefWeb API: http://www.programmableweb.com/api/reliefweb
+
+#possible APIs:
+#Amazon Alexa API
+#IBM Watson???
+#context.io for getting most valuable friends via email analysis
+#indeed for job search after misplacemet by natural disasters
+#Microsoft Azure for cloud computing/creating dev gateways/API management
+#capital one for payment systems
+#or chase, or both.
+
+#Strategy for tomorrow:
+#1. get base functionality done:
+#Android guys get the app design down.
+#backend devs focus on those 4 APIs, additional if necessary
+#frontend figure out website
+#2. testing and implementation.
+#3. addition of extra functionalities
+#4. roll out completed versions.
+
+
+#we are going for the following:
+#good neighbor award: $200 first place, 100 for second
+#general placing, monitors, laptops, tablet, decending order
+#national instruments rapid prototyping award
+#mongoDB hack award: gift cards and trip
