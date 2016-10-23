3c6a911b1523040ba00ff4413d5d47906b8eb706
copperstick6 <copperstick6@gmail.com>
2016-10-23 00:13:14 -0500

    completed the weather module pt 1. Pollen.

--- Everything-Else/weather.py
+++ Everything-Else/weather.py
@@ -5,7 +5,7 @@ import codecs
 
 
 #format of the returned list:
-#dayPartName (nights and days), grassPollenIndex, treePollenIndex, treePollenCategory,
+#dayPartName (nights and days), grassPollenIndex, grassPollenCategory, treePollenIndex, treePollenCategory,
 #Ragweed PollenIndex, RagweedPollenCategory
 def getPollen(days, lat, lon):
     if(((days == 3)or(days == 5)or(days == 7)or(days == 10)or(days == 15))==False):
@@ -20,7 +20,16 @@ def getPollen(days, lat, lon):
     if(webUrl.getcode() == 200):
         str_response = webUrl.read().decode('utf-8')
         obj = json.loads(str_response)
-        for i in obj:
-            list2.append(i["daypartName"][i])
-            list2.append(i["grassPollenIndex"][i])
-            list2.append(i[])
+        i = 0
+        while i < (len(obj["pollenForecast12hour"]["daypartName"])):
+            list2.append(obj["pollenForecast12hour"]["daypartName"][i])
+            list2.append(obj["pollenForecast12hour"]["grassPollenIndex"][i])
+            list2.append(obj["pollenForecast12hour"]["grassPollenCategory"][i])
+            list2.append(obj["pollenForecast12hour"]["treePollenIndex"][i])
+            list2.append(obj["pollenForecast12hour"]["treePollenCategory"][i])
+            list2.append(obj["pollenForecast12hour"]["ragweedPollenIndex"][i])
+            list2.append(obj["pollenForecast12hour"]["ragweedPollenCategory"][i])
+            list1.append(list2)
+            list2 = []
+            i+=1
+    return list1
